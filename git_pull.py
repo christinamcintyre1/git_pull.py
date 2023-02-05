8224fa441cf91bb90f4ecd2a308280e6bcda2005
Christina McIntyre <christinamcintyre1@gmail.com>
2023-02-04 21:26:53 -0700

    Add files via upload

---
+++ git_pull.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+
+# Set the current working directory to the repository root
+repo_root = "/Users/christinamcintyre/react-portfolio-website"
+os.chdir(repo_root)
+
+# Run the git pull command
+result = subprocess.run(['git', 'pull'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=repo_root)
+
+# Check if the pull was successful
+if result.returncode == 0:
+    with open("file.txt", "a") as file:
+        file.write("Update: " + str(result.stdout))
+        print("File updated successfully")
+else:
+    print("Error during git pull: " + str(result.stderr))
